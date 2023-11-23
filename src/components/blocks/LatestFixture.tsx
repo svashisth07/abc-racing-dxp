2f18475933ba87a1b3a43b1f5086910a0a87534c
sandeep vashisth <sandeep@sandeeps-Mac-mini.local>
2023-11-23 14:53:19 +0530

    feat: section bookmark

--- src/components/blocks/LatestFixture.tsx
+++ src/components/blocks/LatestFixture.tsx
@@ -1,4 +1,5 @@
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
+import Link from 'next/link';
 import Fixture, { FixtureProps } from '../client/Fixture';
 
 const LatestFixture = ({
@@ -9,12 +10,15 @@ const LatestFixture = ({
   fixtures: FixtureProps[];
 }) => {
   const t = useTranslations();
+  const locale = useLocale();
   return (
     <div
       id="latest-fixture"
       className="container mx-auto bg-white px-4 md:px-0 py-8"
     >
-      <h2 className="text-xl font-bold mb-4">{title}</h2>
+      <h2 className="text-xl font-bold mb-4">
+        <Link href={`/${locale}#latest-fixture`}>{title}</Link>
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {fixtures.map((fixture) => (
           <Fixture key={fixture.id} {...fixture} dateLabel={t('date')} />
